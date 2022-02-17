90faf4259027634d9bf7e097cf491773a624a9f0
stenvis <vladustenko@gmail.com>
2022-02-17 20:36:35 +0200

    up

--- gl.js
+++ gl.js
@@ -1,6 +1,6 @@
 const API = {
    createProgram,
-   compileShader,
+   createPrograms,
    createBuffer,
    createBuffers,
    createStub,
@@ -18,43 +18,40 @@ const API = {
 }; 
 
 class GL {
-   ctx;
-   shaderTypes = {};
+   gl;
+   programs = {};
 
-   constructor(ctx, list = Object.keys(API)) {
-      this.ctx = ctx;
-      this.shaderTypes.vs = ctx.VERTEX_SHADER;
-      this.shaderTypes.fs = ctx.FRAGMENT_SHADER;
-      this.drawElements = drawElements;
+   constructor(gl, presetsList = Object.keys(API)) {
+      this.gl = gl;
       
-      for (const key of list) 
+      for (const key of presetsList)
          this[key] = API[key].bind(this);
    }
 };
 
 class GLProgram {
-   ctx;
+   gl;
 
-   constructor(vs, fs, ctx) {
-      this.ctx = ctx;
+   constructor(vs, fs, gl) {
+      this.gl = gl;
       this.uniforms = {};
-      this.program = ctx.createProgram();
-      ctx.attachShader(this.program, vs);
-      ctx.attachShader(this.program, fs);
-      ctx.linkProgram(this.program);
-
-      if (!ctx.getProgramParameter(this.program, ctx.LINK_STATUS)) {
-         throw ctx.getProgramInfoLog(this.program);
+      this.program = gl.createProgram();
+      gl.attachShader(this.program, vs);
+      gl.attachShader(this.program, fs);
+      gl.linkProgram(this.program);
+   
+      if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
+         throw gl.getProgramInfoLog(this.program);
       }
 
-      const uniformCount = ctx.getProgramParameter(
+      const uniformCount = gl.getProgramParameter(
          this.program,
-         ctx.ACTIVE_UNIFORMS
+         gl.ACTIVE_UNIFORMS
       );
 
       for (let i = 0; i < uniformCount; i++) {
-         const uniformName = ctx.getActiveUniform(this.program, i).name;
-         this.uniforms[uniformName] = ctx.getUniformLocation(
+         const uniformName = gl.getActiveUniform(this.program, i).name;
+         this.uniforms[uniformName] = gl.getUniformLocation(
          this.program,
          uniformName
          );
@@ -62,29 +59,48 @@ class GLProgram {
   }
 
   use() {
-     this.ctx.useProgram(this.program);
+     this.gl.useProgram(this.program);
   }
 };
 
-function createProgram(shaders, program = {}) {
-   for (const name in shaders['fs']) program[name] = new GLProgram(shaders['vs'].base, shaders['fs'][name], this.ctx);
-   return program;
+const compileShader = (src, type, gl) => {
+   const shader = gl.createShader(type);
+   gl.shaderSource(shader, src);
+   gl.compileShader(shader);
+   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) throw gl.getShaderInfoLog(shader);
+   return shader;
+};
+
+function createProgram(shaders) {
+   const 
+      gl = this.gl,
+      keys = Object.keys(shaders),
+      vs = compileShader(shaders[keys[0]], gl.VERTEX_SHADER, gl);
+
+   for (let i = 1; i < keys.length; i++) {
+      const fs = compileShader(shaders[keys[i]], gl.FRAGMENT_SHADER, gl);
+      this.programs = new GLProgram(vs, fs, gl);
+   };
+};
+
+function createPrograms(programsList) {
+   for (const shaders of programsList) this.createProgram(shaders);
 };
 
 function createBuffer() {
-   return this.ctx.createBuffer();
+   return this.gl.createBuffer();
 };
 
 function createBuffers(collection, list, n) {
    if (!list) {
       for (let i = 0; i < n; i++) {
-         collection.push(this.ctx.createBuffer());
+         collection.push(this.gl.createBuffer());
       };
       return;
    };
 
    list.forEach(name => {
-      collection[name] = this.ctx.createBuffer();
+      collection[name] = this.gl.createBuffer();
    });
 };
 
@@ -97,22 +113,14 @@ function stub() {
    return;
 };
 
-function compileShader(type, source) {
-   const shader = this.ctx.createShader(this.shaderTypes[type]);
-   this.ctx.shaderSource(shader, source);
-   this.ctx.compileShader(shader);
-   if (!this.ctx.getShaderParameter(shader, this.ctx.COMPILE_STATUS)) throw this.ctx.getShaderInfoLog(shader);
-   return shader;
-};
-
 function setUniforms(programs, presets) {
    const types = {
-      '1f': (program, data) => { this.ctx.uniform1f(program, data); },
-      '2f': (program, data) => { this.ctx.uniform2f(program, ...data); },
-      '3f': (program, data) => { this.ctx.uniform3f(program, ...data); },
-      '4f': (program, data) => { this.ctx.uniform4f(program, ...data); },
+      '1f': (program, data) => { this.gl.uniform1f(program, data); },
+      '2f': (program, data) => { this.gl.uniform2f(program, ...data); },
+      '3f': (program, data) => { this.gl.uniform3f(program, ...data); },
+      '4f': (program, data) => { this.gl.uniform4f(program, ...data); },
 
-      '1i': (program, fbo) => { this.ctx.uniform1i(program, fbo[0].bind(fbo[1])); },
+      '1i': (program, fbo) => { this.gl.uniform1i(program, fbo[0].bind(fbo[1])); },
    };
 
    Object.keys(presets).forEach(name => {
@@ -122,12 +130,12 @@ function setUniforms(programs, presets) {
    });
 };
 
-function setBufferData(buffer, data, bufferType = this.ctx.ARRAY_BUFFER) {
-   this.ctx.bindBuffer(bufferType, buffer);
-   this.ctx.bufferData(
+function setBufferData(buffer, data, bufferType = this.gl.ARRAY_BUFFER) {
+   this.gl.bindBuffer(bufferType, buffer);
+   this.gl.bufferData(
       bufferType,
       data,
-      this.ctx.STATIC_DRAW
+      this.gl.STATIC_DRAW
    );
 };
 
@@ -140,31 +148,31 @@ function setBuffersData(scene, buffer, bufferType) {
 };
 
 function enableVertexAttribArray(index = 0) {
-   this.ctx.enableVertexAttribArray(index);
+   this.gl.enableVertexAttribArray(index);
 };
 
 function bindAttribPointer(size, arrType, stride, offset, index = 0) {
-   this.ctx.vertexAttribPointer(index, size, arrType, false, stride, offset);
+   this.gl.vertexAttribPointer(index, size, arrType, false, stride, offset);
 };
 
 function bindBuffer(bufferType, buffer) {
-   this.ctx.bindBuffer(bufferType, buffer);
+   this.gl.bindBuffer(bufferType, buffer);
 };
 
 function bindFBO(bufferType, fbo) {
-   this.ctx.bindFramebuffer(bufferType, fbo);
+   this.gl.bindFramebuffer(bufferType, fbo);
 };
 
 function drawArrays(figureType, offset, n) {
-   this.ctx.drawArrays(figureType, offset, n);
+   this.gl.drawArrays(figureType, offset, n);
 };
 
 function drawElements(figureType, indexCount, indexType, offset) {
-   this.ctx.drawElements(figureType, indexCount, indexType, offset);
+   this.gl.drawElements(figureType, indexCount, indexType, offset);
 };
 
 function clear() {
-   this.ctx.clear(this.ctx.COLOR_BUFFER_BIT);
+   this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 };
 
 export default GL;
